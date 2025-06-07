959565eb0813a27278c7981877e25b9ccdc2e05c
skrompel <saharakrompel@mail.weber.edu>
2025-06-07 16:58:13 -0600

    Add starter files

---
+++ calculate.js
@@ -0,0 +1,21 @@
+"use stric";
+
+document.getElementById('calculateBtn').addEventListener('click', calculateArea);
+document.getElementById('resetBtn').addEventListener('click', resetForm);
+
+function calculateArea( {
+    let radius = document.getElementById('radius').value;
+    
+    if (radius < 0 | radius === '') {
+        document.getElementById('result').innerText = "Please enter a valid radius.";
+        return;
+    }
+
+    let area = Math.PI * radius * radius.value;
+    document.getElementById('result').innerText = "The area is: " + area;
+}
+
+function resetForm() {
+    document.getElementById('radius).value = '';
+    document.getElementById('result').innerText = '';
+
